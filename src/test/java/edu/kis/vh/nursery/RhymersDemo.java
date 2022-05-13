b84eb85f64362907585f56e0f0e49d2ff2660906
Asus <Asus@DESKTOP-IQC9P32>
2022-05-13 15:34:13 +0200

    Dodanie TODO w RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,22 +1,23 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.DefaultCountingOutRhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo
 {
 
-    public static void main(String[] args)
-    {
-        RhymersFactory factory = new DefaultRhymersFactory();
+	public static void main(String[] args)
+	{
+		RhymersFactory factory = new DefaultRhymersFactory();
 
 		testRhymers(factory);
 
 	}
 
-	private static void testRhymers(RhymersFactory factory) {
+	private static void testRhymers(RhymersFactory factory)
+	{
+		//Separate testing of each rhymer could help with finding errors
+		//TODO: test each rhymer separately with specific tests not all identically in one function
 		DefaultCountingOutRhymer[] rhymers =
 		{
 			factory.getStandardRhymer(), factory.getFalseRhymer(),
@@ -37,15 +38,18 @@ class RhymersDemo
 			rhymers[3].countIn(rn.nextInt(20));
 		}
 
-		for (DefaultCountingOutRhymer rhymer : rhymers) {
-			while (!rhymer.callCheck()) {
+		//This isn't really a test just a print out
+		//TODO: delete if unnecessary otherwise move into different function: print
+		for (DefaultCountingOutRhymer rhymer : rhymers)
+		{
+			while (!rhymer.callCheck())
+			{
 				System.out.print(rhymer.countOut() + "  ");
 			}
 			System.out.println();
 		}
 
-		System.out.println("total rejected is "
-				+ ((HanoiRhymer) rhymers[3]).reportRejected());
+		System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
 	}
 
 }
